d9aef23a7f9f3c0abfe1ece923a8a3619d5becfa
Jeonghyeon Park <dotsnangles@gmail.com>
2022-10-24 19:58:13 +0900

    UPDATE: uncleaned_v1 training ready

--- papermill.sh
+++ papermill.sh
@@ -2,3 +2,5 @@
 
 papermill ./acd_binary_trainer.ipynb ./papermill_nbs/acd_binary_trainer.ipynb
 papermill ./asc_binary_trainer.ipynb ./papermill_nbs/asc_binary_trainer.ipynb
+
+# nohup bash papermill.sh > ./papermill_nbs/uncleaned_v1.out
