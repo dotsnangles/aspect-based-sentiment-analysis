347f3370889aa1f97cf9e02c2bcc8e6ecc214e22
Jeonghyeon Park <dotsnangles@gmail.com>
2022-10-26 03:00:35 +0900

    UPDATE: training on progress & some files changes

--- papermill.sh
+++ papermill.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-papermill ./acd_binary_trainer.ipynb ./papermill_nbs/acd_binary_trainer.ipynb
-papermill ./asc_binary_trainer.ipynb ./papermill_nbs/asc_binary_trainer.ipynb
+papermill ./acd_binary_trainer.ipynb ./papermill/acd_binary_trainer.ipynb
+papermill ./asc_binary_trainer.ipynb ./papermill/asc_binary_trainer.ipynb
 
-# nohup bash papermill.sh > ./papermill_nbs/uncleaned_v1.out
+# nohup bash papermill.sh > ./papermill/uncleaned_v2.out
