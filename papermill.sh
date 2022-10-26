2bde65fbd7fe15410b6be1aa7bc81ac7d309e14f
Jeonghyeon Park <dotsnangles@gmail.com>
2022-10-26 17:56:00 +0900

    UPDATE: training started with reduced props

--- papermill.sh
+++ papermill.sh
@@ -3,4 +3,4 @@
 papermill ./acd_binary_trainer.ipynb ./papermill/acd_binary_trainer.ipynb
 papermill ./asc_binary_trainer.ipynb ./papermill/asc_binary_trainer.ipynb
 
-# nohup bash papermill.sh > ./papermill/uncleaned_v3.out
+# nohup bash papermill.sh > ./papermill/uncleaned_v4.out
