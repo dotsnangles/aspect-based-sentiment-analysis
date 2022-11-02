c778fe6f534e7a409d9dea9b5a0da08b5c8ef57c
Jeonghyeon Park <dotsnangles@gmail.com>
2022-11-02 20:00:44 +0900

    UPDATE: back to basics

--- papermill.sh
+++ papermill.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 # papermill ./target_tagger_trainer.ipynb ./papermill/target_tagger_trainer.ipynb
-
 # nohup bash papermill.sh > ./papermill/uncleaned_v11_target_tagger.out
 
-
-
-# papermill ./acd_binary_trainer.ipynb ./papermill/acd_binary_trainer.ipynb
-# papermill ./asc_binary_trainer.ipynb ./papermill/asc_binary_trainer.ipynb
-
-# nohup bash papermill.sh > ./papermill/uncleaned_v11.out
-
-
-
-papermill ./taget_tagger_tabsa_trainer.ipynb ./papermill/taget_tagger_tabsa_trainer.ipynb
-
+# papermill ./taget_tagger_tabsa_trainer.ipynb ./papermill/taget_tagger_tabsa_trainer.ipynb
 # nohup bash papermill.sh > ./papermill/uncleaned_v11_tabsa.out
+
+
+
+
+papermill ./trainer_for_acd_binary.ipynb ./papermill/trainer_for_acd_binary.ipynb
+papermill ./trainer_for_asc_multi.ipynb ./papermill/trainer_for_asc_multi.ipynb
+
+# nohup bash papermill.sh > ./papermill/uncleaned_v13.out
